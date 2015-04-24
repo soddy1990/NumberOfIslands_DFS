49a51872c8c88dfeb657389e49b796c0ba6da7f8
soddy1990 <soddy1990@gmail.com>
2015-04-24 12:37:41 -0400

    Initial Commit

---
+++ NumberOfIslands_DFS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NumberOfIslands_DFS
+//
+//  Created by 黄美强 on 15/4/24.
+//  Copyright (c) 2015年 黄 美强. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
